c207c3c77a4974bcf3b5393a59bfd9e2ee143a63
Gabriela Schlemper <gabschlemper@gmail.com>
2023-03-22 16:43:56 +0000

    Add final pages

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,23 +4,41 @@ import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 function SearchBar() {
+  const [searchTerm, setsearchTerm] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setsearchTerm('');
+    }
+  };
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
         pl: 2,
         boxShadow: 'none',
         mr: { sm: 5 },
+        width: { sm: '350px' },
+        display: 'flex',
+        justifyContent: 'space-between',
       }}
     >
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => {
+          setsearchTerm(e.target.value);
+        }}
       />
       <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
         <Search />
